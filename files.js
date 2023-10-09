6fca4dcc8d02d37bf0db1289d0b7e1a907dddba8
DenisPepper <thech@yandex.ru>
2023-10-09 18:24:42 +0300

    Вынесет чтение файлов в отдельный модуль

--- files.js
+++ files.js
@@ -6,6 +6,7 @@ const content = `About avocado: ${file}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', content);
 */
 
+/*асинхронный способ работы с файловой системой
 const file = fs.readFile('./txt/input.txt', 'utf-8', (error, data) => {
   if (error) return console.log('Error 🐸');
 
@@ -14,3 +15,25 @@ const file = fs.readFile('./txt/input.txt', 'utf-8', (error, data) => {
     console.log('file was write!')
   );
 });
+*/
+export const productsJSON = fs.readFileSync(
+  `${__dirname}/templates/data.json`,
+  'utf8'
+);
+
+export const products = JSON.parse(productsJSON);
+
+export const tmpCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf8'
+);
+
+export const tmpCards = fs.readFileSync(
+  `${__dirname}/templates/template-cards.html`,
+  'utf8'
+);
+
+export const tmpProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf8'
+);
--- index.js
+++ index.js
@@ -2,24 +2,36 @@ import * as fs from 'node:fs';
 import * as http from 'node:http';
 import * as url from 'node:url';
 import { __dirname } from './util.js';
+import {
+  productsJSON,
+  products,
+  tmpCard,
+  tmpCards,
+  tmpProduct,
+} from './files.js';
 
 const IP = '127.0.0.1';
 const PORT = 8000;
-const productsJSON = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
-const products = JSON.parse(productsJSON);
 
 const server = http.createServer((req, res) => {
   const route = req.url;
 
+  //Home page
   if (route === '/' || route === '/catalog') {
     res.end('Catalog page');
+
+    //Product page
   } else if (route === '/product') {
     res.end('Product page');
+
+    //API
   } else if (route === '/api') {
     res.writeHead(404, {
       'Content-type': 'application/json',
     });
     res.end(productsJSON);
+
+    //Not found page
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
