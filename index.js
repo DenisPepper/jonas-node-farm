d9e8be43b5b3f391dc26ef47aa14adf58816fd9c
DenisPepper <thech@yandex.ru>
2023-10-09 14:41:22 +0300

    Запишет новый текстовый файл на диск в ансинхронном стиле

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import * as fs from 'node:fs';
 
+/* синхронный способ работы с файловой сиситемой
 const file = fs.readFileSync('./txt/input.txt', 'utf-8');
 const content = `About avocado: ${file}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', content);
+*/
+
+const file = fs.readFile('./txt/input.txt', 'utf-8', (error, data) => {
+  const content = `About avocado: ${data}.\nCreated on ${Date.now()}\nAsync`;
+  fs.writeFile('./txt/output.txt', content, () => console.log('file was write!'));
+});
