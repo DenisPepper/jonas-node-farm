59a8e4066b4710e5d42f04dd47b1c544f1e3e06a
DenisPepper <thech@yandex.ru>
2023-10-09 15:54:23 +0300

    Добавит обработку роутов

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import * as http from 'node:http';
+import * as url from 'node:url';
 
 const IP = '127.0.0.1';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from server!');
+  const route = req.url;
+
+  if (route === '/' || route === '/catalog') {
+    res.end('Catalog page');
+  } else if (route === '/product') {
+    res.end('Product page');
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
 });
 
 server.listen(PORT, IP, () =>
